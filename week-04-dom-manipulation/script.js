1da765072382fe35889d5fc2046f34220eb9b12d
blbobitz <98281806+blbobitz@users.noreply.github.com>
2022-02-16 18:13:37 -0500

    finished proj4

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -1,20 +1,87 @@
-
-// When this function is called, it takes the values from the inputs and adds
-// them to the outputs.
 function fillOutputs() {
 
-  // Get the adjective input element
-  const adjectiveOneInput = document.getElementById('adjective-one-input');
-  // Get the text from the input element
-  const adjectiveOne = adjectiveOneInput.value;
 
-  // Get the adjective output element
-  const adjectiveOneOutput = document.getElementById('adjective-one-output');
-  // Set the text of the input element
-  adjectiveOneOutput.innerText = adjectiveOne;
+  const pluralnounInput = document.getElementById('pluralnoun-input');
 
-  // Get the output container element
+  const pluralnoun = pluralnounInput.value;
+
+
+  const pluralnounOutput = document.getElementById('pluralnoun-output');
+
+  pluralnounOutput.innerText = pluralnoun;
+
+
   const outputDiv = document.getElementById('output');
-  // Make the output container element visible
+
+  outputDiv.style.display = 'block';
+
+}
+
+
+function fillOutputs1(){
+
+  const pluralnoun2Input = document.getElementById('pluralnoun2-input');
+
+  const pluralnoun2 = pluralnoun2Input.value;
+
+
+  const pluralnoun2Output = document.getElementById('pluralnoun2-output');
+
+  pluralnoun2Output.innerText = pluralnoun2;
+
+
+  const outputDiv = document.getElementById('output');
+
+  outputDiv.style.display = 'block';
+}
+
+function fillOutputs2(){
+
+  const verbInput = document.getElementById('verb-input');
+
+  const verb = verbInput.value;
+
+
+  const verbOutput = document.getElementById('verb-output');
+
+  verbOutput.innerText = verb;
+
+
+  const outputDiv = document.getElementById('output');
+
+  outputDiv.style.display = 'block';
+}
+
+function fillOutputs3(){
+
+  const nounInput = document.getElementById('noun-input');
+
+  const noun = nounInput.value;
+
+
+  const nounOutput = document.getElementById('noun-output');
+
+  nounOutput.innerText = noun;
+
+
+  const outputDiv = document.getElementById('output');
+
+  outputDiv.style.display = 'block';
+}
+
+function fillOutputs4(){
+
+  const ingverbInput = document.getElementById('ingverb-input');
+
+  const ingverb = ingverbInput.value;
+
+
+  const ingverbOutput = document.getElementById('ingverb-output');
+
+  ingverbOutput.innerText = ingverb;
+
+
+  const outputDiv = document.getElementById('output');
+
   outputDiv.style.display = 'block';
 }
